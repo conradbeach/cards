a35da0dd255f06c1060c8eefe70a5e561fb041bb
Conrad Beach <conradbeach@gmail.com>
2016-10-10 09:39:46 -0600

    Update templates.

---
+++ public/javascripts/handlebars_templates.js
@@ -0,0 +1,15 @@
+this["JST"] = this["JST"] || {};
+
+this["JST"]["card"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var helper;
+
+  return container.escapeExpression(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"title","hash":{},"data":data}) : helper)))
+    + "<a href=\"#\">Edit</a>";
+},"useData":true});
+
+this["JST"]["list"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var helper;
+
+  return container.escapeExpression(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : {},{"name":"title","hash":{},"data":data}) : helper)))
+    + "<a href=\"#\">Edit</a><ul></ul>";
+},"useData":true});
