982d876a13788ef14fb6c8aa03c327ab202fb274
Conrad Beach <conradbeach@gmail.com>
2016-10-18 10:14:05 -0600

    Improve clickability of cards and new list link.

--- public/javascripts/handlebars_templates.js
+++ public/javascripts/handlebars_templates.js
@@ -47,9 +47,9 @@ this["JST"]["search_result"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"m
 this["JST"]["simple_card"] = Handlebars.template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : {}, alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "<span class=\"editTitle fa fa-pencil\" aria-hidden=\"true\"></span><a href=\"#"
+  return "<a href=\"#"
     + alias4(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"id","hash":{},"data":data}) : helper)))
-    + "\"><span class=\"title\">"
+    + "\" class=\"title\">"
     + alias4(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"title","hash":{},"data":data}) : helper)))
-    + "</span></a><textarea class=\"editTitleInput hidden\"></textarea>";
+    + "</a><span class=\"editTitle fa fa-pencil\" aria-hidden=\"true\"></span><textarea class=\"editTitleInput hidden\"></textarea>";
 },"useData":true});
